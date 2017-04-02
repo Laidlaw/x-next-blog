3bfb782952d8d1bdc08a0678237a935ecdbff4dc
Alan Laidlaw <alanlaidlaw@reggie-watts.attlocal.net>
2017-04-02 18:29:11 -0500

    trying out build

--- .next/dist/lib/withData.js
+++ .next/dist/lib/withData.js
@@ -50,8 +50,6 @@ var _initStore = require('./initStore');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = '/Users/alanlaidlaw/Desktop/site/with-next/with-apollo/lib/withData.js';
-
 exports.default = function (Component) {
   return function (_React$Component) {
     (0, _inherits3.default)(_class, _React$Component);
@@ -84,16 +82,7 @@ exports.default = function (Component) {
                     break;
                   }
 
-                  app = _react2.default.createElement(_reactApollo.ApolloProvider, { client: client, store: store, __source: {
-                      fileName: _jsxFileName,
-                      lineNumber: 21
-                    }
-                  }, _react2.default.createElement(Component, (0, _extends3.default)({}, props, {
-                    __source: {
-                      fileName: _jsxFileName,
-                      lineNumber: 22
-                    }
-                  })));
+                  app = _react2.default.createElement(_reactApollo.ApolloProvider, { client: client, store: store }, _react2.default.createElement(Component, props));
                   _context.next = 13;
                   return (0, _reactApollo.getDataFromTree)(app);
 
@@ -137,16 +126,7 @@ exports.default = function (Component) {
     (0, _createClass3.default)(_class, [{
       key: 'render',
       value: function render() {
-        return _react2.default.createElement(_reactApollo.ApolloProvider, { client: this.client, store: this.store, __source: {
-            fileName: _jsxFileName,
-            lineNumber: 50
-          }
-        }, _react2.default.createElement(Component, (0, _extends3.default)({}, this.props, {
-          __source: {
-            fileName: _jsxFileName,
-            lineNumber: 51
-          }
-        })));
+        return _react2.default.createElement(_reactApollo.ApolloProvider, { client: this.client, store: this.store }, _react2.default.createElement(Component, this.props));
       }
     }]);
 
--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -30,26 +30,6 @@ var _withData2 = _interopRequireDefault(_withData);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = '/Users/alanlaidlaw/Desktop/site/with-next/with-apollo/pages/index.js?entry';
 exports.default = (0, _withData2.default)(function (props) {
-  return _react2.default.createElement(_App2.default, {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 8
-    }
-  }, _react2.default.createElement(_Header2.default, { pathname: props.url.pathname, __source: {
-      fileName: _jsxFileName,
-      lineNumber: 9
-    }
-  }), _react2.default.createElement(_Submit2.default, {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 10
-    }
-  }), _react2.default.createElement(_PostList2.default, {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 11
-    }
-  }));
+  return _react2.default.createElement(_App2.default, null, _react2.default.createElement(_Header2.default, { pathname: props.url.pathname }), _react2.default.createElement(_Submit2.default, null), _react2.default.createElement(_PostList2.default, null));
 });
